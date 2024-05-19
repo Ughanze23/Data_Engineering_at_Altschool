72f0120e06f07c22445c4699a6a9f770864f5179
Ikenna <ughanzepolycarp@gmail.com>
2024-05-19 14:43:14 +0000

    correct errors in init.sql file table was missing schema name

--- postgres_docker_init/infra_setup/init.sql
+++ postgres_docker_init/infra_setup/init.sql
@@ -3,14 +3,14 @@ CREATE SCHEMA IF NOT EXISTS  staging;
 
 
 -- create usa social security applications name data table
-CREATE table IF NOT EXISTS usa_names (
- state CHAR(2) NULL, -- char(2) for fixed-length 2-digit state codes
-  gender CHAR(1) NULL, -- char(1) for fixed-length sex indicator
+CREATE table IF NOT EXISTS staging.usa_names (
+ state VARCHAR(2) NULL, -- VARCHAR(2) for fixed-length 2-digit state codes
+  gender VARCHAR(1) NULL, -- VARCHAR(1) for fixed-length sex indicator
   year INTEGER NULL,
   name VARCHAR NULL,
   number INTEGER NULL
 
-)
+);
 
 --insert data into table
 COPY staging.usa_names (state, gender, year,name,number)
