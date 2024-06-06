135a6844dbd6eb77bb63a9373240ed395ff36c9e
Ikenna <ughanzepolycarp@gmail.com>
2024-06-06 17:15:23 +0000

    update config file with all params

--- 02_py_gcs_bq/src/config.py
+++ 02_py_gcs_bq/src/config.py
@@ -1,12 +1,11 @@
 DATA_SET_NAME = "etl_basics"  # bigquery dataset name
 PROJECT_ID = "alt-school-423017"  # GC project id
-BUCKECT_NAME = ""  # GCS bucket name
-TABLE_NAME = ""  # bigquery table_name
-GCS_UPLOAD_FILE_PATH = ""  # file path to upload a blob to GCS
-GCS_FILE_NAME = ""  # destination blob name in GCS
+BUCKECT_NAME = "etl_basics"  # GCS bucket name
+TABLE_NAME = ["usa_names","games"]  # bigquery table_name
+GCS_UPLOAD_FILE_PATH = "/workspaces/AltschoolfinalsemesterPortfolio/02_py_gcs_bq/data/usa_names.csv"  # file path to upload a blob to GCS
+GCS_FILE_NAME = ["usa_name.csv","games.json"]  # destination blob name in GCS
 DESTINATION_FILE_NAME = ""  # downloaded file from gcs file path
-BASE_URL = (
-    "https://api.sampleapis.com/playstation"  # URL provided to get request api script
-)
+BASE_URL = "https://api.sampleapis.com/playstation"  # URL provided to get request api script
 TARGET_ENDPOINT = "games"  # goes with the baseURL
-SCHEMA_FILE_PATH = ""  # file path of bigquery table json schema
+SCHEMA_FILE_PATH = ["02_py_gcs_bq/schemas/usa_names_schema.json",
+                    "02_py_gcs_bq/schemas/games_schema.json"]  # file path of bigquery table json schema
