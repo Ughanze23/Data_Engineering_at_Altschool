dfd53e2853c7beffc3b4220ebc31a7212ba85dbe
Ikenna <ughanzepolycarp@gmail.com>
2024-07-28 10:13:25 +0000

    feat: create gcs bucket, datasets and tables in bigquery

--- 05_capstone_project/src/config.py
+++ 05_capstone_project/src/config.py
@@ -1,33 +1,10 @@
-DATA_SET_NAME = "etl_basics"  # bigquery dataset name
+DATA_SET_NAME = "ecommerce"  # bigquery dataset name
+DATA_SET_PREFIX = ['staging','ods']
 PROJECT_ID = "alt-school-423017"  # GC project id
-BUCKECT_NAME = "etl_basics"  # GCS bucket name
-TABLE_NAME = ["usa_names", "games"]  # bigquery table_name
-GCS_UPLOAD_FILE_PATH = "/workspaces/AltschoolfinalsemesterPortfolio/02_py_gcs_bq/data/usa_names.csv"  # file path to upload a blob to GCS
-GCS_FILE_NAME = ["usa_name.csv", "games.json"]  # destination blob name in GCS
-DESTINATION_FILE_NAME = ""  # downloaded file from gcs file path
-BASE_URL = (
-    "https://api.sampleapis.com/playstation"  # URL provided to get request api script
-)
-TARGET_ENDPOINT = "games"  # goes with the baseURL
-SCHEMA_FILE_PATH = [
-    "/workspaces/AltschoolfinalsemesterPortfolio/02_py_gcs_bq/schemas/usa_names_schema.json",
-    "/workspaces/AltschoolfinalsemesterPortfolio/02_py_gcs_bq/schemas/games_schema.json",
-]  # file path of bigquery table json schema
+BUCKECT_NAME = "prj_alt_school_ecommerce"  # GCS bucket name
+TABLE_NAME = ["customers","geolocation",
+              "order_items","orders",
+              "order_payments","order_reviews",
+              "orders","products",
+              "sellers","product_category"]  # bigquery table_name
 
-BQ_FILE_UPLOAD_PATH = (
-    "/workspaces/AltschoolfinalsemesterPortfolio/02_py_gcs_bq/data/usa_names.csv"
-)
-
-# Dictionary to store all table configurations
-table_configs = {
-    "usa_names": {
-        "TABLE_NAME": "usa_names",
-        "GCS_FILE_NAME": "usa_name.csv",
-        "SCHEMA_FILE_PATH": "../schemas/usa_names_schema.json",
-    },
-    "games": {
-        "TABLE_NAME": "games",
-        "GCS_FILE_NAME": "games.json",
-        "SCHEMA_FILE_PATH": "../schemas/games_schema.json",
-    },
-}
--- 05_capstone_project/src/main.py
+++ 05_capstone_project/src/main.py
@@ -3,8 +3,6 @@ from gcs_manager import GcsManager
 from bq_manager import BqManager
 from dotenv import load_dotenv
 import os
-import get_request_api as api
-import json
 
 load_dotenv()
 
@@ -20,58 +18,16 @@ if __name__ == "__main__":
     # create bucket
     gcs_client.create_bucket(bucket_name=c.BUCKECT_NAME)
 
-    # create dataset
-    bq_client.create_dataset(dataset_id=c.DATA_SET_NAME, location="EU")
-
     # create tables
-    bq_client.create_table(
-        schema_file_path=c.table_configs.get("usa_names").get("SCHEMA_FILE_PATH"),
-        dataset_id=c.DATA_SET_NAME,
-        table_id=c.table_configs.get("usa_names").get("TABLE_NAME"),
-    )
-    bq_client.create_table(
-        schema_file_path=c.table_configs.get("games").get("SCHEMA_FILE_PATH"),
-        dataset_id=c.DATA_SET_NAME,
-        table_id=c.table_configs.get("games").get("TABLE_NAME"),
-    )
-
-    # load csv data from system into bigquery table
-    # fetch table schema
-    with open(c.table_configs.get("usa_names").get("SCHEMA_FILE_PATH")) as schema_file:
-        schema = json.load(schema_file)
-
-    # fetch bigquery job config
-    job_config = bq_client.fetch_job_config(file_format="csv", schema=schema)
-
-    # read file as bytes
-    with open(c.BQ_FILE_UPLOAD_PATH, "rb") as file:
-        # upload data to biquery
-        table = f'{c.DATA_SET_NAME}.{c.table_configs.get("usa_names").get("TABLE_NAME")}'  # table name
-        bq_client.upload_data_to_table_from_file(
-            file_obj=file, destination_table=table, job_config=job_config
+    for prefix in c.DATA_SET_PREFIX:
+        # create dataset
+        bq_client.create_dataset(
+            dataset_id=f"{prefix}_{c.DATA_SET_NAME}", location="EU"
         )
 
-    # upload data from API into >> GCS then load into >> Bigquery table
-    # fetch data from API
-    gcs_data = api.get_request_jsonl(url=f"{c.BASE_URL}/{c.TARGET_ENDPOINT}")
-
-    # upload json data into gcs
-    gcs_uri = gcs_client.upload_file_from_filestream(
-        file_obj=gcs_data,
-        bucket_name=c.BUCKECT_NAME,
-        destination_blob_name=c.table_configs.get("games").get("GCS_FILE_NAME"),
-    )
-
-    # load data from gcs into bigquery
-    # fetch table schema
-    with open(c.table_configs.get("games").get("SCHEMA_FILE_PATH")) as schema_file:
-        schema = json.load(schema_file)
-
-    # fetch bigquery job config
-    job_config = bq_client.fetch_job_config(file_format="json", schema=schema)
-
-    # load data into bigquery
-    table = f'{c.DATA_SET_NAME}.{c.table_configs.get("games").get("TABLE_NAME")}'  # table name
-    bq_client.upload_data_to_table_from_uri(
-        gcs_uri=gcs_uri, destination_table=table, job_config=job_config
-    )
+        for table in c.TABLE_NAME:
+            bq_client.create_table(
+                schema_file_path=f"../schemas/{table}_schema.json",
+                dataset_id=f"{prefix}_{c.DATA_SET_NAME}",
+                table_id=f"{table}",
+            )
