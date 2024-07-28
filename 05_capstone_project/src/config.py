e82af3269608384631852eef5137adee9e242e55
Ikenna <ughanzepolycarp@gmail.com>
2024-07-28 14:48:46 +0000

    remove ceation of datalake  bucket for architecture

--- 05_capstone_project/src/config.py
+++ 05_capstone_project/src/config.py
@@ -1,7 +1,6 @@
 DATA_SET_NAME = "ecommerce"  # bigquery dataset name
 DATA_SET_PREFIX = ['staging','ods']
 PROJECT_ID = "alt-school-423017"  # GC project id
-BUCKECT_NAME = "prj_alt_school_ecommerce"  # GCS bucket name
 TABLE_NAME = ["customers","geolocation",
               "order_items","orders",
               "order_payments","order_reviews",
