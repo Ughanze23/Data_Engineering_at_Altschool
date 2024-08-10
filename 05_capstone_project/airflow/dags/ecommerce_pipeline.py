9aaba5d93cef1214ceb05bf7c2a6de2a6e6d2d2f
Ikenna Ughanze <user@MACs-MacBook-Pro.local>
2024-08-10 08:55:24 +0100

    complete airflow dag to move data from postgres to bigquery

--- 05_capstone_project/airflow/dags/ecommerce_pipeline.py
+++ 05_capstone_project/airflow/dags/ecommerce_pipeline.py
@@ -6,8 +6,13 @@ from airflow.operators.empty import EmptyOperator
 from datetime import datetime, timedelta 
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.slack.notifications.slack import send_slack_notification
 
 BUCKECT_NAME = "prj_alt_school_ecommerce"   
+PROJECT_ID="alt-school-423017"
+DATASET="raw_ecommerce"
+
 schemas = {
     "customers" : ["customers"],
  "orders" : ["orders","order_items","order_payments","order_reviews"],
@@ -28,46 +33,81 @@ default_args = {'owner': dag_owner,
         start_date=datetime(2024,6,25),
         schedule_interval= None,
         catchup=False,
-        tags=['ecommerce']
+        tags=['ecommerce'],
+        on_success_callback=[
+        send_slack_notification(
+            slack_conn_id='slack',
+            text="The DAG {{ dag.dag_id }} succeeded",
+            channel="#data-engineering",
+            username="Airflow",
+        )
+    ],
+    on_failure_callback=[
+        send_slack_notification(
+            slack_conn_id='slack',
+            text="The DAG {{ dag.dag_id }} failed",
+            channel="#data-engineering",
+            username="Airflow",
+        )
+    ],
+
     )
 
 def ecommerce_pipeline():
 
-    db_to_gcs = PostgresToGCSOperator(
-                 task_id=f'extract_customers_to_GCS',
-                    postgres_conn_id='prod_db',
-                    sql=f'SELECT * FROM customers.customers;',
-                    bucket=BUCKECT_NAME,
-                    filename=f'data/customers_data.parquet',
-                    export_format='parquet',
-                    gzip=False
-             )
-
-    #test the connection works
-    PostgresOperator_task = PostgresOperator(
-    task_id='PostgresOperator_task',
-    postgres_conn_id='prod_db',
-    sql = """select * from  customers.customers"""
-    )
-    """ 
+    #load postgres tables to gcs
     postgres_to_gcs_tasks = []
     for schema, tables in schemas.items():
         for table in tables:
-            task = PostgresToGCSOperator(
-                 task_id=f'extract_{table}_to_GCS',
+            if table != 'order_reviews':
+                task = PostgresToGCSOperator(
+                    task_id=f'extract_{table}_to_GCS',
                     postgres_conn_id='prod_db',
                     sql=f'SELECT * FROM {schema}.{table};',
                     bucket=BUCKECT_NAME,
-                    filename=f'data/{table}_data.parquet',
-                    export_format='parquet',
-                    gzip=False
-             )
+                    filename=f'data/{table}_data.csv',
+                    export_format='csv',
+                    gzip=False,
+                    )
+            else :
+                task = PostgresToGCSOperator(
+                task_id=f'extract_{table}_to_GCS',
+                postgres_conn_id='prod_db',
+                sql=f'SELECT * FROM {schema}.{table};',
+                bucket=BUCKECT_NAME,
+                filename=f'data/{table}_data.json',
+                export_format='json',
+                    )
         postgres_to_gcs_tasks.append(task)
     
-
-    transfer_customers_postgres_to_biquery = ""
+    #write gcs data to bigquery tables
+    gcs_to_bq_tasks = []
+    for tables in schemas.values():
+        for table in tables :
+            if table != 'order_reviews':
+                task = GCSToBigQueryOperator(
+                    task_id=f'load_{table}_gcs_data_to_bigquery',
+                    bucket=BUCKECT_NAME,
+                    source_objects=f'data/{table}_data.csv',
+                    destination_project_dataset_table=f'{PROJECT_ID}.{DATASET}.{table}',
+                    source_format='CSV',
+                    write_disposition='WRITE_TRUNCATE',
+                    skip_leading_rows=1,
+                    quote_character='"'
+                    )
+            else:
+                task =  GCSToBigQueryOperator(
+                    task_id=f'load_{table}_gcs_data_to_bigquery',
+                    bucket=BUCKECT_NAME,
+                    source_objects=f'data/{table}_data.json',
+                    destination_project_dataset_table=f'{PROJECT_ID}.{DATASET}.{table}',
+                    source_format="NEWLINE_DELIMITED_JSON",
+                    write_disposition="WRITE_TRUNCATE",
+                    autodetect=True,
+                    )
+        gcs_to_bq_tasks.append(task)
     
-    postgres_to_gcs_tasks
-    """
-    db_to_gcs
+    for pg_task in range(len(postgres_to_gcs_tasks)):
+        postgres_to_gcs_tasks[pg_task] >> gcs_to_bq_tasks[pg_task]
+    
 ecommerce_pipeline()
