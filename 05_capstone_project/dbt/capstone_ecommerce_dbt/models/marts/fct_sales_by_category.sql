42b793c9c69b4cc16b2d157c4dd6848e14c85952
Ikenna Ughanze <user@MACs-MacBook-Pro.local>
2024-08-12 09:19:06 +0100

    feat: aggregate the fct_sales_category model

--- 05_capstone_project/dbt/capstone_ecommerce_dbt/models/marts/fct_sales_by_category.sql
+++ 05_capstone_project/dbt/capstone_ecommerce_dbt/models/marts/fct_sales_by_category.sql
@@ -1,1 +1,3 @@
-SELECT * FROM {{ref("int_sales_by_category")}}
+SELECT product_category,sum(total_price) as total_sales FROM {{ref("int_sales_by_category")}}
+GROUP BY product_category
+ORDER BY 2 DESC
