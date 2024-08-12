795c7c80cbff45504e7a7006ee625a1fbc6db306
Ikenna Ughanze <user@MACs-MacBook-Pro.local>
2024-08-12 09:44:49 +0100

    fix issue with null seller_id and seller_zipcode in the fct_orders_by_state model

--- 05_capstone_project/dbt/capstone_ecommerce_dbt/models/intermediate/int_orders_by_state.sql
+++ 05_capstone_project/dbt/capstone_ecommerce_dbt/models/intermediate/int_orders_by_state.sql
@@ -1,14 +1,14 @@
 WITH orders_by_state AS (
 SELECT o.order_id,
-        oi.seller_id,
+        s.seller_id,
         c.customer_id,
         c.customer_zip_code,
         s.seller_zip_code,
-        1 AS total_orders
+        1 AS order_count
     FROM {{ref("stg_orders")}} AS o
-   LEFT JOIN {{ref("stg_order_items")}} AS oi ON oi.order_id = o.order_id
-   LEFT JOIN {{ref("stg_customers")}} AS c ON c.customer_order_id = o.customer_order_id
-   LEFT JOIN {{ref("stg_sellers")}} AS s ON s.seller_id = oi.seller_id
+   JOIN {{ref("stg_order_items")}} AS oi ON oi.order_id = o.order_id
+   JOIN {{ref("stg_customers")}} AS c ON c.customer_order_id = o.customer_order_id
+   JOIN {{ref("stg_sellers")}} AS s ON s.seller_id = oi.seller_id
 )
 
 
