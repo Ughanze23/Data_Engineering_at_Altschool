0205010ae47628b751ffc7dc77f6f53ff8015fff
Ikenna Ughanze <user@MACs-MacBook-Pro.local>
2024-08-12 16:05:15 +0100

    clean the product category columns in stg_product_category model.replace _ with ' '

--- 05_capstone_project/dbt/capstone_ecommerce_dbt/models/sources/olist_ecommerce/products/stg_product_category.sql
+++ 05_capstone_project/dbt/capstone_ecommerce_dbt/models/sources/olist_ecommerce/products/stg_product_category.sql
@@ -4,8 +4,8 @@ with source as (
 renamed as (
     select
         {{ adapter.quote("string_field_0") }} as category_id,
-        {{ adapter.quote("string_field_1") }} as product_category_name,
-        {{ adapter.quote("string_field_2") }} as product_category
+        REPLACE({{ adapter.quote("string_field_1") }},'_', ' ') as product_category_name,
+        REPLACE({{ adapter.quote("string_field_2") }}, '_', ' ') as product_category
 
     from source
 )
